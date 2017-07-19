e6d72886f2d567f22251bced0845612c9886a873
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-19 11:05:19 -0700

    starting fixes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const express = requre('express');
+const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
 const path = require('path');
+
+
 
 
 // TODO: get all animals
